20350f36e514e8b3f529f054342fad4f1c9f4152
Felpo-J <nahuelprado16@gmail.com>
2024-08-25 19:14:14 -0300

    estructura login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = (status) => {
+    setIsAuthenticated(status);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? (
+        <div>
+          <h1>Bienvenido, estás autenticado!</h1>
+        </div>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+      
     </div>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Aquí iría la lógica para autenticar al usuario
+    if (username === "admin" && password === "1234") {
+      onLogin(true);
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Usuario:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Ingresar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
